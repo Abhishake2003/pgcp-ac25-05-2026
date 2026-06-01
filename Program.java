47e3161ff667464638b3c7098ae4c506e51963e8
exam <shubham>
2026-06-01 21:47:52 +0530

    new file

---
+++ Program.java
@@ -0,0 +1,7 @@
+public class Program{
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++){
+            System.out.println("The square of " + i + " is: " + (i*i));
+        }
+    }
+}
